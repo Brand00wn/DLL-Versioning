0a2654b7ae8ec1d8a04be9de1e24ce2aa57dd95a
gbrandao <gbrandao@kpmg.com.br>
2020-11-16 11:39:46 -0300

    Integração com o NGitVersion e teste de DLL

---
+++ NGitVersion/Generated/GlobalAssemblyInfo.cs
@@ -0,0 +1,13 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly:AssemblyCompany                ("TODO Company")]
+[assembly:AssemblyProduct                ("TODO Product")]
+[assembly:AssemblyCopyright              ("TODO Copyright")]
+[assembly:AssemblyTrademark              ("")]
+[assembly:AssemblyCulture                ("")]
+[assembly:AssemblyConfiguration          ("DEBUG")]
+
+[assembly:AssemblyVersion                ("0.0.0.2")]
+[assembly:AssemblyFileVersion            ("0.0.0.2")]
+[assembly:AssemblyInformationalVersion   ("0.0.0.2, Hash 2b79056, BuildConfig DEBUG, HasLocalChange True")]
---
+++ NGitVersion/Generated/GlobalAssemblyInfo.h
@@ -0,0 +1,9 @@
+#define VERSION_COMA   0,0,0,2
+#define VERSION        "0.0.0.2"
+#define FULL_VERSION   "0.0.0.2, Hash 2b79056, BuildConfig DEBUG, HasLocalChange True"
+#define COMPANY        "TODO Company"
+#define PRODUCT        "TODO Product"
+#define COPYRIGHT      "TODO Copyright"
+#define TRADEMARK      "TODO Trademark"
+#define CONFIGURATION  "DEBUG"
+#define CULTURE        ""
